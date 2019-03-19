30b2de7f2110126f2b02f56e298e4904c6c03c18
oneman72hourgamejam <oneman72hourgamejam@gmail.com>
2019-03-19 23:51:09 +0300

    create double buffer

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,39 @@
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dx10.lib")
+#pragma comment(lib, "DxErr.lib")
+#pragma comment(lib, "legacy_stdio_definitions.lib")
+
 #include <windows.h>
+#include <d3d11.h>
+#include <D3DX11.h>
+#include <D3DX10.h>
+#include <DxErr.h>
+#include <xnamath.h>
 
 LPCTSTR WndClassName = "window";
 HWND hwnd = NULL;
 
 const int Width = 800;
 const int Height = 600;
+
+IDXGISwapChain* SwapChain;
+ID3D11Device* d3d11Device;
+ID3D11DeviceContext* d3d11DevCon;
+ID3D11RenderTargetView* renderTargetView;
+
+float red = 0.0f;
+float green = 0.0f;
+float blue = 0.0f;
+int colormodr = 1;
+int colormodg = 1;
+int colormodb = 1;
+
+bool InitializeDirect3d11App(HINSTANCE hInstance);
+void ReleaseObjects();
+bool InitScene();
+void UpdateScene();
+void DrawScene();
 
 bool InitializeWindow(HINSTANCE hInstance, 
 	int ShowWnd,
@@ -29,7 +58,21 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		return 0;
 	}
 
+	if (!InitializeDirect3d11App(hInstance))
+	{
+		MessageBox(0, "Direct Failed", "Error", MB_OK);
+		return 0;
+	}
+
+	if (!InitScene())
+	{
+		MessageBox(0, "Scene Failed", "Error", MB_OK);
+		return 0;
+	}
+
 	messageloop();
+
+	ReleaseObjects();
 
 	return 0;
 }
@@ -49,7 +92,7 @@ bool InitializeWindow(HINSTANCE hInstance,
 	wc.hInstance = hInstance;
 	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wc.lpszMenuName = NULL;
 	wc.lpszClassName = WndClassName;
 	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
@@ -81,6 +124,103 @@ bool InitializeWindow(HINSTANCE hInstance,
 	return true;
 }
 
+bool InitializeDirect3d11App(HINSTANCE hInstance)
+{
+	HRESULT hr;
+
+	DXGI_MODE_DESC bufferDesc;
+
+	ZeroMemory(&bufferDesc, sizeof(DXGI_MODE_DESC));
+
+	bufferDesc.Width = Width;
+	bufferDesc.Height = Height;
+	bufferDesc.RefreshRate.Numerator = 60;
+	bufferDesc.RefreshRate.Denominator = 1;
+	bufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	bufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	bufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+
+	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	swapChainDesc.BufferDesc = bufferDesc;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.OutputWindow = hwnd;
+	swapChainDesc.Windowed = TRUE;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	hr = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE,
+		NULL, NULL, NULL, NULL, D3D11_SDK_VERSION, &swapChainDesc,
+		&SwapChain, &d3d11Device, NULL, &d3d11DevCon);
+
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, DXGetErrorDescription(hr), "SwapChain", MB_OK);
+		return 0;
+	}
+
+	ID3D11Texture2D* BackBuffer;
+	hr = SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D),
+		(void**)&BackBuffer);
+
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, DXGetErrorDescription(hr), "BackBuffer", MB_OK);
+		return 0;
+	}
+
+	hr = d3d11Device->CreateRenderTargetView(BackBuffer, NULL,
+		&renderTargetView);
+	BackBuffer->Release();
+
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, DXGetErrorDescription(hr), "D3D11Device", MB_OK);
+		return 0;
+	}
+
+	d3d11DevCon->OMSetRenderTargets(1, &renderTargetView, NULL);
+}
+
+void ReleaseObjects()
+{
+	SwapChain->Release();
+	d3d11Device->Release();
+	d3d11DevCon->Release();
+}
+
+bool InitScene()
+{
+	return true;
+}
+
+void UpdateScene()
+{
+	red += colormodr * 0.00005f;
+	green += colormodg * 0.00005f;
+	blue += colormodb * 0.00005f;
+
+	if (red >= 1.0f || red <= 0.0f)
+		colormodr *= -1;
+	if (green >= 1.0f || green <= 0.0f)
+		colormodg *= -1;
+	if (blue >= 1.0f || blue <= 0.0f)
+		colormodb *= -1;
+}
+
+void DrawScene()
+{
+	D3DXCOLOR bgColor(red, green, blue, 1.0f);
+
+	d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
+
+	SwapChain->Present(0, 0);
+}
+
 int messageloop()
 {
 	MSG msg;
@@ -98,7 +238,8 @@ int messageloop()
 		}
 		else
 		{
-			//game code
+			UpdateScene();
+			DrawScene();
 		}
 	}
 	
